605513308d47b38fbd775557ba176202dc8f3dc9
Ronith Raj <ronithrajkopperla@gmail.com>
2026-01-03 23:40:57 +0530

    Added vercel endpoint in cors.

--- healthcare-clinic-backend/src/main/java/com/healthcare/clinic/config/CorsConfig.java
+++ healthcare-clinic-backend/src/main/java/com/healthcare/clinic/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+//                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+                .allowedOrigins("https://healthcare-app-ui-ivory.vercel.app/")
                 .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Location")
